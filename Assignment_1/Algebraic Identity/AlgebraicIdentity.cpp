e761e1a25d27ac9b0f5ce54288c444594cf4dbd4
Frattolillo Mattia <it.m.frattolillo@outlook.com>
2025-03-19 15:51:22 +0100

    Assignment "Algebraic Identity" Structure
    Add basic structure to make first assignment "Algebraic Identity"

---
+++ Assignment_1/Algebraic Identity/AlgebraicIdentity.cpp
@@ -0,0 +1,74 @@
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+//-----------------------------------------------------------------------------
+// AlgebraicIdentity implementation
+//-----------------------------------------------------------------------------
+// No need to expose the internals of the pass to the outside world - keep
+// everything in an anonymous namespace.
+namespace {
+  // New PM implementation
+  struct AlgebraicIdentity: PassInfoMixin<AlgebraicIdentity> {
+
+  bool runOnBasicBlock(BasicBlock &B) {
+      return true;
+    }
+
+
+  bool runOnFunction(Function &F) {
+    bool Transformed = false;
+
+    for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
+      if (runOnBasicBlock(*Iter)) {
+        Transformed = true;
+      }
+    }
+
+    return Transformed;
+  }
+
+    // Main entry point, takes IR unit to run the pass on (&F) and the
+    // corresponding pass manager (to be queried if need be)
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+      if (!runOnFunction(F))
+        return PreservedAnalyses::none();
+      return PreservedAnalyses::all();
+  }
+
+
+    // Without isRequired returning true, this pass will be skipped for functions
+    // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+    // all functions with optnone.
+    static bool isRequired() { return true; }
+};
+} // namespace
+
+//-----------------------------------------------------------------------------
+// New PM Registration
+//-----------------------------------------------------------------------------
+llvm::PassPluginLibraryInfo getAlgebraicIdentityPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "AlgebraicIdentity", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "algebric-identity") {
+                    FPM.addPass(AlgebraicIdentity());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+// This is the core interface for pass plugins. It guarantees that 'opt' will
+// be able to recognize TestPass when added to the pass pipeline on the
+// command line, i.e. via '-passes=test-pass'
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getAlgebraicIdentityPluginInfo();
+}
